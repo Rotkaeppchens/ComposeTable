f9bb28092616539a2964af343dd3ad18a887b565
Lukas Deutscher <L.Deutscher@gmx.net>
2023-08-22 20:41:55 +0200

    Added black color behind led display on status screen

--- src/main/kotlin/ui/screens/StatusScreen.kt
+++ src/main/kotlin/ui/screens/StatusScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -77,6 +78,9 @@ fun LedLight(
         border = BorderStroke(1.dp, MaterialTheme.colorScheme.onSurface),
         modifier = modifier
             .size(dotSize)
+            .drawBehind {
+                drawCircle(Color.Black)
+            }
     ) {  }
 }
 
