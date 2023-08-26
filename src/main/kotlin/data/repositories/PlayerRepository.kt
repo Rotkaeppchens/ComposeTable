be4156ad0404b1fdc9f40483d115f49a9f91ac60
Lukas Deutscher <L.Deutscher@gmx.net>
2023-08-26 16:18:47 +0200

    Moved player management to the database

--- src/main/kotlin/data/repositories/PlayerRepository.kt
+++ src/main/kotlin/data/repositories/PlayerRepository.kt
@@ -2,10 +2,18 @@ package data.repositories
 
 import data.BaseConfig
 import data.LedColor
+import data.database.tables.PlayerTable
 import data.entities.Player
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.deleteWhere
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.update
 
 class PlayerRepository(
     private val config: BaseConfig
@@ -13,9 +21,8 @@ class PlayerRepository(
     private val scope = CoroutineScope(Dispatchers.Default)
 
     private val _players: MutableStateFlow<List<Player>> = MutableStateFlow(emptyList())
-    val players: StateFlow<List<Player>> = _players.map { playerList ->
-        playerList.sortedBy { it.id }
-    }.stateIn(scope, SharingStarted.Eagerly, emptyList())
+    val players: StateFlow<List<Player>>
+        get() = _players
 
     private val _playerMap: MutableStateFlow<Array<Int?>> = MutableStateFlow(
         value = Array(config.getSides().size) { null }
@@ -30,39 +37,86 @@ class PlayerRepository(
         }.toMap()
     }.stateIn(scope, SharingStarted.Eagerly, emptyMap())
 
-    private var maxId = 0
+    init {
+        scope.launch {
+            loadPlayersFromDatabase()
+        }
+    }
+
+    private fun loadPlayersFromDatabase() {
+        transaction {
+            _players.update {
+                PlayerTable.selectAll().map {
+                    Player(
+                        id = it[PlayerTable.id],
+                        name = it[PlayerTable.name],
+                        color = LedColor(
+                            red = it[PlayerTable.colorRed],
+                            green = it[PlayerTable.colorGreen],
+                            blue = it[PlayerTable.colorBlue],
+                            alpha = it[PlayerTable.colorAlpha]
+                        )
+                    )
+                }
+            }
+        }
+    }
 
     fun addPlayer() {
-        _players.update {
-            val newList = it.toMutableList()
-            newList.add(
-                    Player(
-                    id = maxId++,
-                    name = "New Player",
-                    color = LedColor(1.0, 1.0, 1.0, 1.0)
-                )
-            )
-            newList
+        scope.launch {
+            transaction {
+                PlayerTable.insert {
+                    it[name] = "New Player"
+                    it[colorRed] = 1.0
+                    it[colorGreen] = 1.0
+                    it[colorBlue] = 1.0
+                    it[colorAlpha] = 1.0
+                }
+            }
+
+            loadPlayersFromDatabase()
         }
     }
 
     fun removePlayer(playerId: Int) {
-        _playerMap.update {
-            val newArr = it.copyOf()
+        scope.launch {
+            transaction {
+                PlayerTable.deleteWhere { id eq playerId }
+            }
 
-            newArr.forEachIndexed { i, id ->
-                if (id == playerId) {
-                    newArr[i] = null
+            loadPlayersFromDatabase()
+        }
+    }
+
+    fun setPlayerColor(playerId: Int, color: LedColor) {
+        scope.launch {
+            transaction {
+                PlayerTable.update({ PlayerTable.id eq playerId }) {
+                    it[colorRed] = color.red
+                    it[colorGreen] = color.green
+                    it[colorBlue] = color.blue
+                    it[colorAlpha] = color.alpha
                 }
             }
 
-            newArr
+            loadPlayersFromDatabase()
         }
-        _players.update { playerList ->
-            val newList = playerList.toMutableList()
-            newList.removeAll { it.id == playerId }
-            newList
+    }
+
+    fun setPlayerName(playerId: Int, name: String): Boolean {
+        if (name.isBlank()) return false
+
+        scope.launch {
+            transaction {
+                PlayerTable.update({ PlayerTable.id eq playerId }) {
+                    it[this.name] = name
+                }
+            }
+
+            loadPlayersFromDatabase()
         }
+
+        return true
     }
 
     fun setPlayerSide(sideId: Int, playerId: Int?) {
@@ -74,39 +128,5 @@ class PlayerRepository(
             newArr[sideId] = playerId
             newArr
         }
-    }
-
-    fun setPlayerColor(playerId: Int, color: LedColor) {
-        _players.update { playerList ->
-            val newList = playerList.toMutableList()
-            val i = newList.indexOfFirst { it.id == playerId }
-
-            if (i != -1) {
-                val player = newList[i]
-                newList[i] = player.copy(
-                    color = color
-                )
-            }
-
-            newList
-        }
-    }
-
-    fun setPlayerName(playerId: Int, name: String): Boolean {
-        if (name.isBlank()) return false
-
-        _players.update {
-            val newList = it.toMutableList()
-            val i = newList.indexOfFirst { it.id == playerId }
-
-            if (i != -1) {
-                val player = newList[i]
-                newList[i] = player.copy(name = name)
-            }
-
-            newList
-        }
-
-        return true
     }
 }
