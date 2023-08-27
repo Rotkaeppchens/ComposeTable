fb25ea3854fe687110b67bfe2b774e06d48b5138
Lukas Deutscher <L.Deutscher@gmx.net>
2023-08-27 21:37:16 +0200

    Updated the settings screen to edit the module priorities

--- src/main/kotlin/ui/screens/SettingsScreen.kt
+++ src/main/kotlin/ui/screens/SettingsScreen.kt
@@ -1,27 +1,30 @@
 package ui.screens
 
-import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Info
-import androidx.compose.material.icons.outlined.Tune
-import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.material.icons.outlined.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import com.github.mbelling.ws281x.LedStripType
+import androidx.compose.ui.unit.dp
 import data.BaseConfig
+import data.entities.ModuleConfig
 import org.koin.compose.koinInject
-import ui.theme.AppTheme
 import view_models.SettingsViewModel
-import kotlin.time.Duration
+
+enum class SettingsNavState {
+    INFO,
+    MODULE_CONFIG
+}
 
 @Composable
 fun SettingsScreen(
@@ -32,6 +35,13 @@ fun SettingsScreen(
 
     SettingsScreen(
         config = uiState.config,
+        moduleConfigList = uiState.moduleConfigList,
+        onModuleMove = { moduleId, newIndex ->
+            viewModel.moveModule(moduleId, newIndex)
+        },
+        onModuleEnable = { moduleId, enabled ->
+            viewModel.setModuleEnabled(moduleId, enabled)
+        },
         modifier = modifier
     )
 }
@@ -39,56 +49,202 @@ fun SettingsScreen(
 @Composable
 fun SettingsScreen(
     config: BaseConfig.Companion.Config,
+    moduleConfigList: List<ModuleConfig>,
+    onModuleMove: (id: String, newIndex: Int) -> Unit,
+    onModuleEnable: (moduleId: String, enabled: Boolean) -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val (navState, setNavState) = remember { mutableStateOf(SettingsNavState.INFO) }
+
     Column(
         modifier = modifier
     ) {
-        Row {
-            Column {
-                Text("LED Service:", fontWeight = FontWeight.Bold)
-                Text(
-                    "Init Strip: ${config.ledService.initStrip}\n" +
-                            "LED Count: ${config.ledService.ledCount}\n" +
-                            "GPIO: ${config.ledService.gpioPin}\n" +
-                            "Frequency: ${config.ledService.frequencyHz}\n" +
-                            "DMA: ${config.ledService.dma}\n" +
-                            "Brightness: ${config.ledService.brightness}\n" +
-                            "PWM channel: ${config.ledService.pwmChannel}\n" +
-                            "Invert: ${config.ledService.invert}\n" +
-                            "Type: ${config.ledService.stripType}\n" +
-                            "Clear on exit: ${config.ledService.clearOnExit}\n"
-                )
-            }
-            Column {
-                Text("Table Config:", fontWeight = FontWeight.Bold)
-                Text(
-                    "Break Points: ${config.tableConfig.breakPoints}\n" +
-                            "Loop sleep time: ${config.tableConfig.loopSleepTime}\n"
-                )
-                Text("Interface Config:", fontWeight = FontWeight.Bold)
-                Text(
-                    "Dark Theme: ${config.interfaceConfig.useDarkTheme}\n" +
-                            "Maximise Window: ${config.interfaceConfig.maximiseWindow}\n" +
-                            "Renderer: ${config.interfaceConfig.renderer}\n"
+        AnimatedContent(
+            targetState = navState,
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+        ) { state ->
+            when(state) {
+                SettingsNavState.INFO -> InfoScreen(config)
+                SettingsNavState.MODULE_CONFIG -> ModuleList(
+                    moduleConfigList = moduleConfigList,
+                    onModuleMove = onModuleMove,
+                    onModuleEnable = onModuleEnable
                 )
             }
         }
-        Spacer(
+        NavBar(
+            navState = navState,
+            setNavState = setNavState
+        )
+    }
+}
+
+@Composable
+fun InfoScreen(
+    config: BaseConfig.Companion.Config,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        modifier = modifier
+            .padding(8.dp)
+            .fillMaxSize()
+    ) {
+        Column {
+            Text("LED Service:", fontWeight = FontWeight.Bold)
+            Text(
+                "Init Strip: ${config.ledService.initStrip}\n" +
+                        "LED Count: ${config.ledService.ledCount}\n" +
+                        "GPIO: ${config.ledService.gpioPin}\n" +
+                        "Frequency: ${config.ledService.frequencyHz}\n" +
+                        "DMA: ${config.ledService.dma}\n" +
+                        "Brightness: ${config.ledService.brightness}\n" +
+                        "PWM channel: ${config.ledService.pwmChannel}\n" +
+                        "Invert: ${config.ledService.invert}\n" +
+                        "Type: ${config.ledService.stripType}\n" +
+                        "Clear on exit: ${config.ledService.clearOnExit}\n"
+            )
+        }
+        Column {
+            Text("Table Config:", fontWeight = FontWeight.Bold)
+            Text(
+                "Break Points: ${config.tableConfig.breakPoints}\n" +
+                        "Loop sleep time: ${config.tableConfig.loopSleepTime}\n"
+            )
+            Text("Interface Config:", fontWeight = FontWeight.Bold)
+            Text(
+                "Dark Theme: ${config.interfaceConfig.useDarkTheme}\n" +
+                        "Maximise Window: ${config.interfaceConfig.maximiseWindow}\n" +
+                        "Renderer: ${config.interfaceConfig.renderer}\n"
+            )
+        }
+    }
+}
+
+@Composable
+fun ModuleList(
+    moduleConfigList: List<ModuleConfig>,
+    onModuleMove: (id: String, newIndex: Int) -> Unit,
+    onModuleEnable: (moduleId: String, enabled: Boolean) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val state = rememberLazyListState()
+
+    LazyColumn(
+        state = state,
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(8.dp),
+        modifier = modifier
+    ) {
+        items(moduleConfigList, key = { it.id }) { item ->
+            val currentIndex = moduleConfigList.indexOf(item)
+
+            ModuleItem(
+                item = item,
+                onModuleEnable = {
+                    onModuleEnable(item.id, it)
+                },
+                onModuleMove = { up ->
+                    val newIndex = if (up) {
+                        currentIndex - 1
+                    } else {
+                        currentIndex + 1
+                    }
+                    onModuleMove(item.id, newIndex)
+                },
+                moveUpEnabled = currentIndex != 0,
+                moveDownEnabled = currentIndex != moduleConfigList.lastIndex,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    }
+}
+
+@Composable
+fun ModuleItem(
+    item: ModuleConfig,
+    onModuleMove: (up: Boolean) -> Unit,
+    onModuleEnable: (Boolean) -> Unit,
+    moveUpEnabled: Boolean,
+    moveDownEnabled: Boolean,
+    modifier: Modifier = Modifier
+) {
+    val switchIcon: (@Composable () -> Unit)? = if (item.enabled) {
+        {
+            Icon(
+                imageVector = Icons.Outlined.Check,
+                contentDescription = null,
+                modifier = Modifier.size(SwitchDefaults.IconSize)
+            )
+        }
+    } else {
+        null
+    }
+
+    val backgroundColor = if (item.enabled) {
+        MaterialTheme.colorScheme.primaryContainer
+    } else {
+        MaterialTheme.colorScheme.secondaryContainer
+    }
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .clip(MaterialTheme.shapes.medium)
+            .background(backgroundColor)
+            .padding(start = 16.dp, end = 8.dp)
+    ) {
+        Text(
+            text = item.priority.toString(),
+            fontWeight = FontWeight.Light,
+            fontStyle = FontStyle.Italic,
+            style = MaterialTheme.typography.titleMedium
+        )
+        Spacer(Modifier.width(16.dp))
+        FilledTonalIconButton(
+            enabled = moveUpEnabled,
+            onClick = { onModuleMove(true) }
+        ) {
+            Icon(
+                imageVector = Icons.Outlined.ArrowUpward,
+                contentDescription = null
+            )
+        }
+        FilledTonalIconButton(
+            enabled = moveDownEnabled,
+            onClick = { onModuleMove(false) }
+        ) {
+            Icon(
+                imageVector = Icons.Outlined.ArrowDownward,
+                contentDescription = null
+            )
+        }
+        Spacer(Modifier.width(16.dp))
+        Text(
+            text = item.id,
+            style = MaterialTheme.typography.titleLarge,
             modifier = Modifier.weight(1f)
         )
-        NavBar()
+        Switch(
+            checked = item.enabled,
+            onCheckedChange = onModuleEnable,
+            thumbContent = switchIcon
+        )
     }
 }
 
 @Composable
 fun NavBar(
+    navState: SettingsNavState,
+    setNavState: (SettingsNavState) -> Unit,
     modifier: Modifier = Modifier
 ) {
     NavigationBar(modifier = modifier) {
         NavigationBarItem(
-            selected = true,
-            onClick = {},
+            selected = navState == SettingsNavState.INFO,
+            onClick = { setNavState(SettingsNavState.INFO) },
             icon = {
                 Icon(
                     imageVector = Icons.Outlined.Info,
@@ -100,8 +256,8 @@ fun NavBar(
             }
         )
         NavigationBarItem(
-            selected = false,
-            onClick = {},
+            selected = navState == SettingsNavState.MODULE_CONFIG,
+            onClick = { setNavState(SettingsNavState.MODULE_CONFIG) },
             icon = {
                 Icon(
                     imageVector = Icons.Outlined.Tune,
@@ -111,38 +267,6 @@ fun NavBar(
             label = {
                 Text("Modules")
             }
-        )
-    }
-}
-
-@Preview
-@Composable
-fun SettingsScreenPreview() {
-    AppTheme {
-        SettingsScreen(
-            config = BaseConfig.Companion.Config(
-                ledService = BaseConfig.Companion.LedService(
-                    initStrip = true,
-                    ledCount = 286,
-                    gpioPin = 10,
-                    frequencyHz = 800000,
-                    dma = 0,
-                    brightness = 255,
-                    pwmChannel = 18,
-                    invert = false,
-                    stripType = LedStripType.WS2811_STRIP_GBR,
-                    clearOnExit = true,
-                ),
-                tableConfig = BaseConfig.Companion.TableConfig(
-                    breakPoints = listOf(1, 2, 3, 4),
-                    loopSleepTime = Duration.ZERO
-                ),
-                interfaceConfig = BaseConfig.Companion.InterfaceConfig(
-                    useDarkTheme = true,
-                    maximiseWindow = false,
-                    renderer = BaseConfig.Companion.ComposeRenderer.DEFAULT
-                )
-            )
         )
     }
 }
--- src/main/kotlin/view_models/SettingsViewModel.kt
+++ src/main/kotlin/view_models/SettingsViewModel.kt
@@ -1,20 +1,47 @@
 package view_models
 
 import data.BaseConfig
-import kotlinx.coroutines.flow.MutableStateFlow
+import data.ModuleController
+import data.entities.ModuleConfig
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import view_models.base.ViewModel
 
 class SettingsViewModel(
-    baseConfig: BaseConfig
+    baseConfig: BaseConfig,
+    private val moduleController: ModuleController
 ) : ViewModel() {
-    val uiState: StateFlow<UiState> = MutableStateFlow(
+    val uiState: StateFlow<UiState> = moduleController.moduleConfigList.map {
         UiState(
-            config = baseConfig.config
+            config = baseConfig.config,
+            moduleConfigList = it
         )
-    )
+    }.stateIn(viewModelScope, SharingStarted.Eagerly, UiState(
+        config = baseConfig.config,
+        moduleConfigList = emptyList()
+    ))
+
+    fun setModuleEnabled(moduleId: String, enabled: Boolean) = moduleController.setModuleEnabled(moduleId, enabled)
+
+    fun moveModule(moduleId: String, newIndex: Int) {
+        val moduleList = moduleController.moduleConfigList.value.toMutableList()
+
+        val moduleIndex = moduleList.indexOfFirst { it.id == moduleId }
+        val removedModule = moduleList.removeAt(moduleIndex)
+
+        moduleList.add(newIndex, removedModule)
+
+        moduleController.setModulePriorityList(
+            moduleList.mapIndexed { i, module ->
+                module.id to i
+            }.toMap()
+        )
+    }
 
     data class UiState (
-        val config: BaseConfig.Companion.Config
+        val config: BaseConfig.Companion.Config,
+        val moduleConfigList: List<ModuleConfig>
     )
 }
