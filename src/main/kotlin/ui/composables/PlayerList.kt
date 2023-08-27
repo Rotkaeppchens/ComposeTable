2b9048c3f79eaf6d1d7d1f8d68f7359e43a8ae49
Lukas Deutscher <L.Deutscher@gmx.net>
2023-08-27 09:51:22 +0200

    Added drag scroll to the player list

--- src/main/kotlin/ui/composables/PlayerList.kt
+++ src/main/kotlin/ui/composables/PlayerList.kt
@@ -3,18 +3,25 @@ package ui.composables
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberDraggableState
+import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import data.LedColor
 import data.entities.Player
+import kotlinx.coroutines.launch
 import ui.theme.AppTheme
 import ui.toColor
 
@@ -25,11 +32,23 @@ fun PlayerList(
     activePlayerId: Int? = null,
     modifier: Modifier = Modifier
 ) {
+    val lazyListState = rememberLazyListState()
+    val scope = rememberCoroutineScope()
+
     LazyColumn(
         contentPadding = PaddingValues(8.dp),
+        state = lazyListState,
         modifier = modifier
             .width(200.dp)
             .background(MaterialTheme.colorScheme.secondaryContainer)
+            .draggable(
+                orientation = Orientation.Vertical,
+                state = rememberDraggableState { delta ->
+                    scope.launch {
+                        lazyListState.scrollBy(-delta)
+                    }
+                },
+            )
     ) {
         items(players, key = { it.id }) {
             PlayerItem(
