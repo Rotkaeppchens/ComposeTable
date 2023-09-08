6c0481a8081d0014897e287c42e387c51adaaca0
Lukas Deutscher <L.Deutscher@gmx.net>
2023-09-08 23:31:07 +0200

    Renamed the LedClock to LedAnimationClock
    This is to make it clearer what clock is for

--- src/main/kotlin/data/LedAnimationClock.kt
+++ src/main/kotlin/data/LedAnimationClock.kt
@@ -4,7 +4,7 @@ import androidx.compose.runtime.BroadcastFrameClock
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 
-object LedClock {
+object LedAnimationClock {
     val clock = BroadcastFrameClock()
 
     val animationScope: CoroutineScope = CoroutineScope(Dispatchers.Default + clock)
