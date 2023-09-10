cf3e6bf32d4b9a636e6fcefcbdb3a2b10d22401a
Lukas Deutscher <L.Deutscher@gmx.net>
2023-09-10 16:18:33 +0200

    Added missing import for material divider

--- src/main/kotlin/ui/navigation/NavBar.kt
+++ src/main/kotlin/ui/navigation/NavBar.kt
@@ -11,10 +11,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Close
-import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationRail
-import androidx.compose.material3.NavigationRailItem
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
