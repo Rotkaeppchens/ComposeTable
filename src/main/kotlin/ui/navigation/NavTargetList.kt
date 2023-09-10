49529f98971276a1d58e4d553c851ee0aef982a8
Lukas Deutscher <L.Deutscher@gmx.net>
2023-09-10 14:32:57 +0200

    Added a list of all navigation targets with the new nav target dsl

---
+++ src/main/kotlin/ui/navigation/NavTargetList.kt
@@ -0,0 +1,48 @@
+package ui.navigation
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.*
+import ui.navigation.builder.NavListBuilder
+import ui.screens.*
+
+val NavTargetList = NavListBuilder {
+    group("Status") {
+        target {
+            title = "Status"
+            icon = Icons.Outlined.BlurOn
+            screen = { StatusScreen(modifier = it) }
+            isDefaultTarget = true
+        }
+    }
+    group("Setup") {
+        target {
+            title = "Players"
+            icon = Icons.Outlined.Person
+            screen = { PlayerScreen(modifier = it) }
+        }
+        target {
+            title = "Table"
+            icon = Icons.Outlined.Hexagon
+            screen = { TableSetupScreen(modifier = it) }
+        }
+    }
+    group("Screens") {
+        target {
+            title = "Timer"
+            icon = Icons.Outlined.Timer
+            screen = { TimerScreen(modifier = it) }
+        }
+        target {
+            title = "Health"
+            icon = Icons.Outlined.Bloodtype
+            screen = { HealthScreen(modifier = it) }
+        }
+    }
+    group("Settings") {
+        target {
+            title = "Settings"
+            icon = Icons.Outlined.Settings
+            screen = { SettingsScreen(modifier = it) }
+        }
+    }
+}
